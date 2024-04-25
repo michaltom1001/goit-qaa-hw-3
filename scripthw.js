37a72dbf3c772aba4822e4dafc0ce58366c9e84a
michaltom1001 <149585727+michaltom1001@users.noreply.github.com>
2024-04-25 19:33:59 +0200

    Add files via upload

---
+++ scripthw.js
@@ -0,0 +1,12 @@
+const fruits = ["Jabłko", "Banan", "Pomarańcza", "Winogrona", "Arbuz"];
+
+
+
+const numbers = [1, 3, 4, 6, 7, 9];
+
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+    if (number > 5) {
+        console.log(number);
+    }
+}
